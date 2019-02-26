a95de0d04b7668ac4284a9e8e82b7a311c811960
Alain Vaucher <alain.vaucher@benevolent.ai>
2019-02-26 17:33:45 +0000

    update best from chembl

--- best_from_chembl/optimizer.py
+++ best_from_chembl/optimizer.py
@@ -1,8 +1,10 @@
 import heapq
 from typing import List, Optional, Tuple
 
+import joblib
 from guacamol.goal_directed_generator import GoalDirectedGenerator
 from guacamol.scoring_function import ScoringFunction
+from joblib import delayed
 
 from .chembl_file_reader import ChemblFileReader
 
@@ -12,29 +14,21 @@ class BestFromChemblOptimizer(GoalDirectedGenerator):
     Goal-directed molecule generator that will simply look for the most adequate molecules present in a file.
     """
 
-    def __init__(self, smiles_reader: ChemblFileReader) -> None:
-        self.smiles_reader = smiles_reader
+    def __init__(self, smiles_reader: ChemblFileReader, n_jobs: int) -> None:
+        self.pool = joblib.Parallel(n_jobs=n_jobs)
+        # get a list of all the smiles
+        self.smiles = [s for s in smiles_reader]
+
+    def top_k(self, smiles, scoring_function, k):
+        joblist = (delayed(scoring_function.score)(s) for s in smiles)
+        scores = self.pool(joblist)
+        scored_smiles = list(zip(scores, smiles))
+        scored_smiles = sorted(scored_smiles, key=lambda x: x[0], reverse=True)
+        return [smile for score, smile in scored_smiles][:k]
 
     def generate_optimized_molecules(self, scoring_function: ScoringFunction, number_molecules: int,
                                      starting_population: Optional[List[str]] = None) -> List[str]:
         """
         Will iterate through the reference set of SMILES strings and select the best molecules.
-
-        It will create a heap and keep it to the required size so that minimal memory is used.
         """
-        top_molecules: List[Tuple[float, str]] = []
-
-        for smiles in self.smiles_reader:
-            score = scoring_function.score(smiles)
-
-            # Put molecule and corresponding score in a tuple that allows for appropriate comparison operator for the heap.
-            item = (score, smiles)
-
-            if len(top_molecules) < number_molecules:
-                heapq.heappush(top_molecules, item)
-            else:
-                # Equivalent to a push, then a pop, but faster
-                # NB: pop removes the smallest value, i.e. in this case the molecule with the lowest score.
-                heapq.heappushpop(top_molecules, item)
-
-        return [x[1] for x in top_molecules]
+        return self.top_k(self.smiles, scoring_function, number_molecules)
