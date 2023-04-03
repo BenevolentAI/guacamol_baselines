cc2b2b250f45b77c6bf54b2a1fb36babeff115ee
Joshua Meyers <joshua_meyers@rocketmail.com>
2023-04-03 16:34:24 +0100

    rename sampling

--- frag_gt/frag_gt/tests/src/test_frag_scorers.py
+++ frag_gt/frag_gt/tests/src/test_frag_scorers.py
@@ -7,25 +7,24 @@ FRAG_COUNT_TUPLES = [("[2*]Cc1cc(O)cc(O[4*])c1", 2),
                      ("[2*]CC(N[4*])C(C)C", 1)]
 
 
-def test_sampling():
+def test_frag_scorer():
     # Given
-    fragment_sampler = FragScorer(scorer="random", sort=False)
-    query_frag = Chem.MolFromSmiles("[2*]CC(N[4*])C(C)C")
+    fragment_scorer = FragScorer(scorer="random", sort=False)
 
     # When
-    scored_frags = fragment_sampler.score(gene_frag_list=FRAG_COUNT_TUPLES)
+    scored_frags = fragment_scorer.score(gene_frag_list=FRAG_COUNT_TUPLES)
 
     # Then
     assert len(scored_frags) == len(FRAG_COUNT_TUPLES)
 
 
-def test_sampling_counts_scorer_and_sort():
+def test_frag_scorer_counts_and_sort():
     # Given
-    fragment_sampler = FragScorer(scorer="counts", sort=True)
+    fragment_scorer = FragScorer(scorer="counts", sort=True)
     query_frag = Chem.MolFromSmiles("[2*]CC(N[4*])C(C)C")
 
     # When
-    scored_frags = fragment_sampler.score(gene_frag_list=FRAG_COUNT_TUPLES, query_frag=query_frag)
+    scored_frags = fragment_scorer.score(gene_frag_list=FRAG_COUNT_TUPLES, query_frag=query_frag)
 
     # Then
     frag_smiles, scores = zip(*scored_frags)
@@ -33,25 +32,25 @@ def test_sampling_counts_scorer_and_sort():
     assert scores == tuple(sorted(original_counts, reverse=True))
 
 
-def test_sampling_afp_scorer():
+def test_frag_scorer_afp():
     # Given
-    fragment_sampler = FragScorer(scorer="afps", sort=True)
+    fragment_scorer = FragScorer(scorer="afps", sort=True)
     query_frag = Chem.MolFromSmiles("[2*]CC(N[4*])C(C)C")
 
     # When
-    scored_frags = fragment_sampler.score(gene_frag_list=FRAG_COUNT_TUPLES[:1], query_frag=query_frag)
+    scored_frags = fragment_scorer.score(gene_frag_list=FRAG_COUNT_TUPLES[:1], query_frag=query_frag)
 
     # Then
     assert len(scored_frags) == 1
 
 
-def test_sampling_ecfp4_scorer():
+def test_frag_scorer_ecfp4():
     # Given
-    fragment_sampler = FragScorer(scorer="ecfp4", sort=True)
+    fragment_scorer = FragScorer(scorer="ecfp4", sort=True)
     query_frag = Chem.MolFromSmiles("[2*]CC(N[4*])C(C)C")
 
     # When
-    scored_frags = fragment_sampler.score(gene_frag_list=FRAG_COUNT_TUPLES, query_frag=query_frag)
+    scored_frags = fragment_scorer.score(gene_frag_list=FRAG_COUNT_TUPLES, query_frag=query_frag)
 
     # Then
     assert len(scored_frags) == 3
